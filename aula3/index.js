46d290bcad0ba4926727bd9d6ceb08a83ba47049
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2021-06-07 18:08:54 -0300

    Aula 3 e Aula 4 - Console.log e comentários

---
+++ aula3/index.js
@@ -0,0 +1,13 @@
+// console.log
+console.log("Hello 'World'"); 
+console.log('Hello "World"'); 
+console.log(`'Hello' "World"`); 
+
+console.log(123456, 15.85)
+
+console.log('Meu nome é "Amanda". Estou aprendendo Javascript às',  18, 'h')
+
+// comentários
+console.log("Hello 'World'"); // aqui tem um comentário
+/*console.log('Hello "World"'); 
+console.log(`'Hello' "World"`); */
