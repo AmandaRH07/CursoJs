363780c87c0e454f3bd6fd9c94f3bc7873a21f89
AmandaRH07 <amandahass02@gmail.com>
2021-06-28 20:15:53 -0300

    Alert, Confirm e Prompt

---
+++ aula11/js/index.js
@@ -0,0 +1,14 @@
+alert('Calculadora');
+
+const nome = prompt("Digite seu nome: ");
+
+let numero1 =  prompt("Insira o numero1: ");
+let numero2 =  prompt("Insira o numero2: ");
+
+confirm("Deseja fazer a soma? ")
+
+numero1 = Number(numero1);
+numero2 = Number(numero2);
+
+const resultado = numero1 + numero2;
+alert(`${nome} digitou ${numero1} + ${numero2} = ${resultado}`);
