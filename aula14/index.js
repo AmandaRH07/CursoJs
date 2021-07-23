dc8535d175fab8b1a92108cb66bbd047d69c37f4
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2021-07-23 20:49:10 -0300

    Numeros e exercicios com numeros

---
+++ aula14/index.js
@@ -0,0 +1,23 @@
+let num1 = 10.8767887687687; //number
+let num2 = 2.5; //number
+
+console.log(num1.toString() + num2);
+console.log(num1.toFixed(2))
+
+// Objeto Math
+
+let num3 = 9.88437874;
+
+let num4 = Math.floor(num3);
+console.log(num4);
+
+num4 = Math.ceil(num3);
+console.log(num4);
+
+console.log(Math.max(1,2,3,4,5,6,7,8,9));
+console.log(Math.min(1,2,3,4,5,6,7,8,9));
+console.log(Math.random());
+
+console.log(Math.pow(2,10)); // elevado
+console.log(2**10);
+console.log(num2 ** 0.5); // raiz quadrada
